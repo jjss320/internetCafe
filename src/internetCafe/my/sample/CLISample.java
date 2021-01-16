8912a0c30f875df0b0478dd0f88494b2a90a4c0f
cabinKim <cavinjun0320@gmail.com>
2021-01-16 15:01:38 +0900

    name prompt 구현 추가

--- src/internetCafe/my/sample/CLISample.java
+++ src/internetCafe/my/sample/CLISample.java
@@ -13,6 +13,7 @@ import org.jline.terminal.TerminalBuilder;
 import org.jline.utils.AttributedStringBuilder;
 import org.jline.utils.AttributedStyle;
 
+import internetCafe.my.cli.CliAuth;
 import internetCafe.my.cli.CliCommands;
 import internetCafe.my.cli.DescriptionGenerator;
 
@@ -30,6 +31,7 @@ public class CLISample {
 	private static CommandLine cmd;
 	private static CliCommands commands;
 	private static DescriptionGenerator descriptionGenerator;
+	private static CliAuth auth = CliAuth.getInstance();
 
 	public static void main(String[] args) {
 		AnsiConsole.systemInstall();
@@ -49,17 +51,23 @@ public class CLISample {
 			new TailTipWidgets(reader, descriptionGenerator::commandDescription, 5, TipType.COMPLETER);
 
 			// start the shell and process input until the user quits with Ctrl-D
-			String prompt = "prompt> ";
+			String prompt = "";
 			String rightPrompt = null;
 			String line;
 			while (true) {
 				try {
+					if(auth.isLogin()) {
+						prompt = auth.getLoginName() + "> ";
+					} else {
+						prompt = "prompt> ";
+					}
+					
 					// 명령어 읽기
 					line = reader.readLine(prompt, rightPrompt, (MaskingCallback) null, null);
 					if (line.matches("^\\s*#.*")) {
 						continue;
 					}
-
+					
 					// 명령어 파싱
 					ParsedLine pl = reader.getParser().parse(line, 0);
 					String[] arguments = pl.words().toArray(new String[0]);
